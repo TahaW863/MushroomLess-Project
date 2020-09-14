795ef0ac421e752d971c493d83f6ca72c6bd7e6f
Taha Talal <tahatlal@gmai.com>
2020-09-14 17:43:52 +0300

    initial commit

--- AWS/MushroomLess/Web/MushroomSite/assets/js/req.js
+++ AWS/MushroomLess/Web/MushroomSite/assets/js/req.js
@@ -1,10 +1,39 @@
-axios
-  .all([
-    axios.get(
-      'SomeAPI'
-    ),
-  ])
-  .then((responseArr) => {
-    //this will be executed only when all requests are complete
-    console.log('Date created: ', responseArr[0].data);
-  });
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    var x = new String(myObj.body);
+    var value = 0;
+    var yeah = false;
+    for (var i = 0; i < x.length; i++) {
+      if (x[i] == 'e') {
+        yeah = true;
+      }
+    }
+    if (yeah) {
+      value = 0;
+    } else {
+      if (x.length > 6) {
+        if (parseInt(x[3]) >= 5) {
+          value = 1;
+        } else {
+          value = 0;
+        }
+      } else {
+        if (parseInt(x[1]) == 1) value = 1;
+      }
+    }
+    var Res;
+    if (value == 1) {
+      Res = 'YES!';
+    } else {
+      Res = 'No!';
+    }
+    document.getElementById('toggel').innerHTML = Res;
+    console.log(x);
+    document.getElementById('score').innerHTML = myObj.body;
+  }
+};
+xmlhttp.open('GET', 'SOME-LAMBDA-API', true);
+xmlhttp.send();
+//#document.getElementById("toggel").innerHTML = "Tutorix";
